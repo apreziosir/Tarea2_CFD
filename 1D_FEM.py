cdb91b22528e88740168812305122694126fdf25
apreziosir <apreziosir@unal.edu.co>
2018-03-28 16:35:11 -0500

    assembling FEM matrices

--- 1D_FEM.py
+++ 1D_FEM.py
@@ -9,6 +9,9 @@ Marzo de 2018
 """
 
 import numpy as np
+import scipy.sparse as SP
+import matplotlib.pyplot as plt
+from matplotlib import style
 
 # ==============================================================================
 # Variable declaration
@@ -43,4 +46,71 @@ nconn = np.zeros((ne, 3))
 nconn[:, 0] = n0
 nconn[:, 1] = n1
 nconn[:, 2] = n2
+
+# ==============================================================================
+# Building matrix and RHS for solving
+# ==============================================================================
+
+# Initializing global matrix and global RHS
+dLHS = SP.lil_matrix((nn, nn))
+RHS = np.zeros(nn)
+
+# Loop over elements to assemble diffusion matrix
+
+plt.ion()
+plt.figure(1, figsize=(11, 8.5))
+style.use('ggplot')
+
+for ielt in range(0, ne):
+    
+    # Creating element diffusion matrix, assuming element size h    
+    eLHS = ((Dx * h) / 3) * np.array([[7/4, -2, 1/4],[-2, 4, -2], [1/4, \
+           -2, 7/4]])
+    
+    # Assemble to global LHS matrix
+    # Loop over values in element matrix (i,j) and assemble to global
+    # locations (I,J)
+    for i in range(0, 3):
+        I = nconn[ielt, i]
+        
+        for j in range(0, 3):
+            
+            J = nconn[ielt, j]
+            
+            dLHS[I, J] = dLHS[I, J] + eLHS[i, j]
+            
+            plt.clf()
+            plt.spy(dLHS)
+            plt.draw()
+            plt.pause(0.2)
+
+# Loop over elements to assemble mass matrix
+            
+mLHS = SP.lil_matrix((nn, nn))
+
+plt.ion()
+plt.figure(2, figsize=(11, 8.5))
+style.use('ggplot')
+
+for ielt in range(0, ne):
+    
+    # Creating element diffusion matrix, assuming element size h    
+    eLHS = (h / 15) * np.array([[2, 1, -1/2],[1, 8, 1], [-1/2, 1, 2]])
+    
+    # Assemble to global mLHS matrix
+    # Loop over values in element matrix (i,j) and assemble to global
+    # locations (I,J)
+    for i in range(0, 3):
+        I = nconn[ielt, i]
+        
+        for j in range(0, 3):
+            
+            J = nconn[ielt, j]
+            
+            mLHS[I, J] = mLHS[I, J] + eLHS[i, j]
+            
+            plt.clf()
+            plt.spy(mLHS)
+            plt.draw()
+            plt.pause(0.2)
 
